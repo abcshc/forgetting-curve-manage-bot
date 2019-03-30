0a33122dd7f6288aefc3972261bd332981916953
HeeChul So <thrrkthrrk@gmail.com>
2019-03-30 15:03:55 +0900

    CHORE: 스프링 프로젝트 생성

---
+++ src/main/kotlin/com/sangcow/fc/managebot/ManageBotApplication.kt
@@ -0,0 +1,11 @@
+package com.sangcow.fc.managebot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManageBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<ManageBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/sangcow/fc/managebot/ManageBotApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.sangcow.fc.managebot
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ManageBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
